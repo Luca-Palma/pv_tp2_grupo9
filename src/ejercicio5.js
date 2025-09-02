aa5216e09d863056fdf8a48a16d086ab415a13a9
Luca Palma <luca202010@gmail.com>
2025-09-01 22:48:56 -0300

    Pagina de Inicio y cambios ejercicio5.js LucaPalma

--- src/ejercicio5.js
+++ src/ejercicio5.js
@@ -3,5 +3,8 @@ document.getElementById("calcular").onclick = function() {
     pago = Number(document.getElementById("pago").value);
     nombre = document.getElementById("nombre").value;
     let resultado = horas * pago;
-    document.getElementById("resultado").innerText =nombre+" tu salario es de: "+resultado+"$";
+    if (horas > 160) {
+        resultado = resultado * 1.2;
+    }
+    document.getElementById("resultado").innerText = nombre + " tu salario es de: " + resultado + "$";
 }
